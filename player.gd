8f9ab78278a34c82acf601fa85e41fb593ec1fc7
AidenGillis <aidengillis.pro@gmail.com>
2025-09-25 19:07:27 -0400

    Collision bug fixed, New environment, and trees

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 600.0
+@export var speed: float =225.0
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
