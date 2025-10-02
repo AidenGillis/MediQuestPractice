46af9ad2cd80bfb76565fc6df34e944c7bc3727d
AidenGillis <aidengillis.pro@gmail.com>
2025-10-02 17:07:08 -0400

    Thank you Nathan

--- enemy.gd
+++ enemy.gd
@@ -1,25 +1,28 @@
 class_name Enemy1 
 extends CharacterBody2D
 
-@export var speed: float = 100
+@export var speed: float = 25
 
 var start_position
 var target: Player
 
 func _ready():
 	start_position
+	target = %Player
 
 func update_velocity():
-	if !target: return
-	
-	var direction = (target.global_position - global_position).normalized()
-	var new_velocity = direction.normalized() * speed
-	
+	if(global_position.distance_to(target.global_position)<100):
+		var direction = (target.global_position - global_position)
+		velocity = direction.normalized() * speed
+	else:
+		velocity = Vector2(0,0);
 	
 func _physics_process(_delta: float) -> void:
 	update_velocity()
 	move_and_slide()
+	
 
-func _on_follow_area_body_entered(body):
-	if body is Player:
-		target = body
+#func _on_follow_area_body_entered(body):
+	#print(body);
+	#if body is Player:
+		#target = body
