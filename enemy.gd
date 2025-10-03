a5a6dde1a8a620ca5cb40a0e2a114da7675f4d57
AidenGillis <aidengillis.pro@gmail.com>
2025-10-02 23:16:31 -0400

    no more collision bug and y-sorting for entities, working on y-sorting for trees too

--- enemy.gd
+++ enemy.gd
@@ -1,18 +1,18 @@
 class_name Enemy1 
 extends CharacterBody2D
 
-@export var speed: float = 25
+@export var speed: float = 35
 
 var start_position
 var target: Player
 
 func _ready():
-	start_position
+	start_position = global_position
 	target = %Player
 
 func update_velocity():
-	if(global_position.distance_to(target.global_position)<100):
-		var direction = (target.global_position - global_position)
+	if(global_position.distance_to(%Player.global_position)<175):
+		var direction = (%Player.global_position - global_position)
 		velocity = direction.normalized() * speed
 	else:
 		velocity = Vector2(0,0);
@@ -21,8 +21,5 @@ func _physics_process(_delta: float) -> void:
 	update_velocity()
 	move_and_slide()
 	
-
-#func _on_follow_area_body_entered(body):
-	#print(body);
-	#if body is Player:
-		#target = body
+#func /pls(100) - Tiffani 
+#Mc is the best <3
