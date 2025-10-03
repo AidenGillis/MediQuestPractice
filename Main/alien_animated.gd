4d45256317f0c19d810571b991aab3cdced53dfa
AidenGillis <aidengillis.pro@gmail.com>
2025-10-03 10:54:14 -0400

    alien animated enemy/more y-sorting smoothing

---
+++ Main/alien_animated.gd
@@ -0,0 +1,26 @@
+class_name Alien
+extends CharacterBody2D
+
+@export var speed: float = 35
+@onready var anim = $AnimatedSprite2D
+var start_position
+var target: Player
+
+func _ready():
+	start_position = global_position
+	target = %Player
+
+
+func update_velocity():
+	if(global_position.distance_to(%Player.global_position)<175):
+		var direction = (%Player.global_position - global_position)
+		velocity = direction.normalized() * speed
+		anim.play("Repeat")
+	else:
+		velocity = Vector2(0,0);
+		anim.stop()
+	
+func _physics_process(_delta: float) -> void:
+	update_velocity()
+	move_and_slide()
+	
