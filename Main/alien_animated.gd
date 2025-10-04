5951b2a85838372b50fa84ee30df634aef283613
AidenGillis <aidengillis.pro@gmail.com>
2025-10-03 20:57:54 -0400

    small animation change on alien

--- Main/alien_animated.gd
+++ Main/alien_animated.gd
@@ -15,9 +15,11 @@ func update_velocity():
 	if(global_position.distance_to(%Player.global_position)<175):
 		var direction = (%Player.global_position - global_position)
 		velocity = direction.normalized() * speed
-		anim.play("Repeat")
 	else:
 		velocity = Vector2(0,0);
+	if(global_position.distance_to(%Player.global_position)<75):
+		anim.play("Repeat")
+	else:
 		anim.stop()
 	
 func _physics_process(_delta: float) -> void:
